d3f0924008479b43a9cb801ec11657850f6e84bc
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-09-14 17:52:08 +0300

    send data to power bi throught API

--- web_scraper/myapp/views.py
+++ web_scraper/myapp/views.py
@@ -5,6 +5,14 @@ import json
 from django.shortcuts import get_object_or_404
 from myapp.services.article_service import create_article, get_subject_from_wikipedia
 from myapp.models import Article
+import json
+from django.http import JsonResponse
+from django.db import connection
+import requests
+
+
+
+POWER_BI_API_URL = "https://api.powerbi.com/beta/6bb41fe4-40a3-4a10-b6cd-38278e78b21a/datasets/44ad7af8-e468-4704-b899-1f07d10cf1e1/rows?experience=power-bi&key=Cim%2FpvRhvtCyHRpOCFtKDDsmLrvUN52yAL21Ig5K6InXlu2tSIejo6B5be8dZuzyPithZG%2BaNFylt1qORMoNLA%3D%3D"
 
 @csrf_exempt
 def create_article_view(request):
@@ -59,4 +67,36 @@ def get_article_by_id_view(request, article_id):
     
     return JsonResponse({"error": "Invalid request method"}, status=405)
 
-# Create your views here.
+@csrf_exempt
+def push_data_to_power_bi(request):
+    if request.method == 'POST':
+        try:
+            articles = Article.objects.all()
+
+            articles_list = []
+            for article in articles:
+                articles_list.append({
+                    "title": article.title,
+                    "paragraph1": article.paragraph1[:50],  # Limit to 50 characters
+                    "paragraph2": article.paragraph2[:50]   # Limit to 50 characters
+                })
+
+            json_data = json.dumps(articles_list)
+
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            response = requests.post(POWER_BI_API_URL, headers=headers, data=json_data)
+
+            if response.status_code == 200:
+                return JsonResponse({"message": "Data sent successfully!"}, status=200)
+            else:
+                return JsonResponse({"error": f"Failed to send data. Status code: {response.status_code}"}, status=500)
+        
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"error": "Invalid request method"}, status=405)
+
+
--- web_scraper/web_scraper/urls.py
+++ web_scraper/web_scraper/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from myapp.views import create_article_view,get_subject_from_wikipedia_view, get_all_articles_view,get_article_by_id_view
+from myapp.views import create_article_view,get_subject_from_wikipedia_view, get_all_articles_view,get_article_by_id_view, push_data_to_power_bi
 
 urlpatterns = [
     path("create-article/", create_article_view, name='create_article'),
     path("get-subject-from-wiki/",get_subject_from_wikipedia_view, name="get_subject_from_wikipedia_view" ),
     path("get-all-articles/", get_all_articles_view, name="get_all_articlesW"),
     path('get-article-by-id/<int:article_id>/', get_article_by_id_view, name='get_article_by_id'),
+    path('send-data-to-power-bi', push_data_to_power_bi, name='send_data_to_power_bi_service'),
 ]
